ac21440aedcc7e8a7bf43147f2901f7251aa2c27
Adriana Sanchez <adrievelyn@gmail.com>
2019-09-15 01:53:08 -0300

    agregando archivos

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Adriana
-        </p>
+        </p>git checkout
         <a
           className="App-link"
           href="https://reactjs.org"
